042121c06dd2c0022629196bac41bc2c69740604
Guilherme Roscoe <62852591+Guilherme-Roscoe@users.noreply.github.com>
2025-12-17 10:02:04 -0300

    Add visual status updates for IP checks
    Enhanced IP checking process with visual feedback using Rich library.

--- DonutStarter.py
+++ DonutStarter.py
@@ -1,6 +1,7 @@
 import time
 from rich.console import Console
 from rich.spinner import Spinner
+from rich.panel import Panel
 import asyncio
 from blacklist_checker import scan_ip, export_csv, export_json
 
@@ -8,17 +9,42 @@ ip_list_text = open("ip_list.txt" , "r").read()
 ip_list = ip_list_text.splitlines()
 
 ip_limpos = []
+todos_ip = {}
 
 inicio = time.time()
 
 console = Console()
 
+
 for ip in ip_list:
-    data = asyncio.run(scan_ip(ip))
 
-    for r in data["results"]:
-        print(f"{r['blacklist']:45} {r['status']:12} {r['time']}s")
+    with console.status(f"[bold green] Checking: {ip} "):
+        data = asyncio.run(scan_ip(ip))
 
-    print(f"\nIP: {data['ip']}")
-    print(f"Listagens: {data['listed_count']}")
-    print(f"Risco: {data['risk']}\n")
+    # Don't show the RBLs Panel if check only one address
+    if len(ip_list) == 1:
+        log_text = '\n'.join([f"{r['blacklist']:40} {r['status']:12} {r['time']:15}s" for r in data['results']])
+        
+        console.print(
+            Panel(
+                log_text,
+                title="Tested RBLs",
+                border_style="green"
+            )
+        )
+
+    #for r in data["results"]:
+    #    print(f"{r['blacklist']:45} {r['status']:12} {r['time']}s")
+
+    listadas = [ l['blacklist'] for l in data['listed_rbl']]
+
+    #register clean IP addresses
+    if int(data['listed_count']) == 0:
+        ip_limpos.append(ip)
+
+    console.print(f"[bold green]\nIP: {data['ip']}\nRisco: {data['risk']}\nListagens: {data['listed_count']}[/bold green][bold red]\n{' | '.join(listadas)}\n\n[/bold red]")
+
+fim = time.time()
+tempo_exec = fim - inicio
+if len(ip_list) > 1:
+    console.print(f"[bold green]Checked addresses: {len(ip_list)}\nClean IP addresses: {len(ip_limpos)}\nExecution time: {tempo_exec:.2f}[/bold green]")
